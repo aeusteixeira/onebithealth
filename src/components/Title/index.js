b5ff79a8a9037b7cd19b2f6a5c03da0d96d55be3
Matheus Teixeira <contato.matheusteixeira@gmail.com>
2023-08-29 21:03:13 -0300

    Finalizado app

---
+++ src/components/Title/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text, View } from "react-native";
+import styles from "./styles";
+
+export default function Title() {
+    return (
+        <View style={styles.boxTitle}>
+            <Text style={styles.textTitle}>
+                ONEBITHEALTH
+            </Text>
+        </View>
+    );
+}
